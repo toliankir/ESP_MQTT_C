d3149d9654be1376f527a790a3ef787c0fbbcdd3
Anatoliy <toliankir@gmail.com>
2019-10-15 08:33:26 +0300

    Change publisher name

--- sketch_oct14a.ino
+++ sketch_oct14a.ino
@@ -11,7 +11,8 @@ const int mqtt_port = 17447;
 const char* mqtt_username = "fjwkxsvo";
 const char* mqtt_password = "0uLbMzS4c4mr";
 long lastMsg = 0;
-
+char devName[50];
+  
 ESP8266WebServer server(80);
 DHTesp dht;
 WiFiClient wifiClient;
@@ -63,12 +64,10 @@ void apSaveAP(){
   }
 
 void initAP(){
-  char apName[50];
-  sprintf(apName, "ESP_%d", system_get_chip_id());
   Serial.print("Starting access point (");
-  Serial.print(apName);
+  Serial.print(devName);
   Serial.println(")...");
-  WiFi.softAP(apName);
+  WiFi.softAP(devName);
   IPAddress myIP = WiFi.softAPIP();
   Serial.print("AP IP adress:");
   Serial.println(myIP);
@@ -104,7 +103,7 @@ bool initWifiClient(){
 void initMqtt(){
     if (client.connect("esp", mqtt_username, mqtt_password)) {
     Serial.println("MQTT connect");
-    client.publish("outTopic","hello world");
+//    client.publish("outTopic","hello world");
 //    client.subscribe("inTopic");
   }
 }
@@ -113,6 +112,7 @@ void setup() {
   // put your setup code here, to run once:
   //  EEPROM.begin(512);
   //  EEPROM.get(0, wifiData);
+  sprintf(devName, "ESP_%d", system_get_chip_id());
   delay(1000);
   Serial.begin(115200);
   if (initWifiClient()) {
@@ -129,14 +129,15 @@ void loop() {
   // put your main code here, to run repeatedly:
   server.handleClient();
   long now = millis();
-  if (now - lastMsg > 5000) {
+  if (now - lastMsg > 10000) {
     lastMsg = now;
     int humidity = dht.getHumidity();
     int temperature = dht.getTemperature();
     String msg = "{\"humidity\": " + String(humidity) +", \"temperature\": "+ String(temperature) +" }";
     char buf[msg.length()];
     msg.toCharArray(buf, msg.length()+1);
-    client.publish("outTopic", buf);
+//    client.publish(devName, buf);
+    client.publish("ESP/sensor", buf);
   }
 
 }
